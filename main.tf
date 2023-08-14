9886eb6a8c1c743c8705ee0d41658ffba28d61cd
salah <salah@gmail.com>
2023-08-14 11:13:49 +0530

    edited

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "###########"
+  access_key = "####@@@@@@@@@"
   secret_key = "YYMS+####################+a39b##G"
 }
